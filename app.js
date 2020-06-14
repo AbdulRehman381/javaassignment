483d47f0a620089de7e5741e17bed88af7fe6695
abdul rehman <ranaabdulrehman902@gmail.com>
2020-06-14 08:53:05 +0500

    javaassignment GIT

---
+++ app.js
@@ -0,0 +1,91 @@
+ // Chapter # 1 Task # 1
+
+// alert("Hello World");
+  
+// Chapter # 1 Task # 2
+
+//var password=prompt("Enter the Password");
+
+// alert("Error! please enter a valid password");
+    
+// Chapter # 1 Task # 3
+
+// alert("Welcome to Js Land \n Happy Coding!");
+
+// Chapter # 1 Task # 4
+
+// alert("Welcome to js land...");
+
+// alert("Welcome to js land...\n prevent this page from creating addional dialogue.");
+
+// Chapter # 1 Task # 5
+
+// console.log("Hello.... i can run js through my web browser's console");
+
+// Chapter # 2 Task # 1
+
+// var username;
+
+
+// Chapter # 2 Task # 2
+
+// var myName="Abdul Rehman";
+
+// Chapter # 2 Task # 3
+
+// var message="Hello World";
+// alert(message);
+
+// Chapter # 2 Task # 4
+
+// var name="jhone Doe";
+// var age="15 years";
+// var edu="Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(edu);
+
+// Chapter # 2 Task # 5
+
+// var a="PIZZA";
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Chapter # 2 Task # 6
+
+// var email="abdulrehman@gmail.com";
+// alert(email);
+
+// Chapter # 2 Task # 7
+
+// var book="I am trying to learn from the book A smarter way to learn javaScript";
+// alert(book);
+
+// Chapter # 2 Task # 8
+
+// document.write("Yah!I can write HTML content through JavaScript");
+
+// Chapter # 2 Task # 9
+
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" );
+
+// Chapter # 3 Task # 1
+
+// var age="I am 15 years old";
+// alert(age);
+
+// Chapter # 3 Task # 2
+
+// var val ="You have visited this site 14 times";
+// alert(val);
+
+// Chapter # 3 Task # 3
+
+// document.write("My birth year is 1990 <br> Data type declared in number");
+
+// Chapter # 3 Task # 4
+
+var name="Jhon Doe ";
+var product="ordered 5-T Shirt ";
+var quantity="XYZ clothing store";
+document.write(name , product,quantity);
